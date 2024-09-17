2acb131b34d26ecc0e8a3976ef1559f2aef57679
abhay <abhaym1205@gmail.com>
2024-09-17 14:25:35 +0530

    updated eslint

--- url-shortener-service/index.js
+++ url-shortener-service/index.js
@@ -15,7 +15,8 @@ const app = express();
 // adding middleware
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://slink-nginx.onrender.com", "http://slink-nginx.onrender.com"],
+    // origin: "*",
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   })
